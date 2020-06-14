486f64b146822e9bea40a2798f9b72c40210a0f4
Aminessid <essidmohamedamin@gmail.com>
2020-06-14 16:42:15 +0000

    add social media icons at the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import menuIcon from '../images/icons/menuIcon.png';
 import {Link} from 'react-router-dom';
+import FaceBookIcon from '../images/icons/facebook.png';
+import EmailIcon from '../images/icons/email.png';
+import LinkedinIcon from '../images/icons/linkedin.png'
 
 function Navbar() {
   const [open, setOpen] = useState(false);
@@ -15,8 +18,10 @@ function Navbar() {
                 <span className='nav-item'>
                   <Link to={() => `/كل-الأصناف${Math.floor(Math.random()*1000)}`} className='nav-item__link'>كل المسلسلات </Link>
                 </span>
-                <span className='nav-item'>
-                  <Link to='#' className='nav-item__link'> اتصل بنا</Link>
+                <span className=' nav-item socialMedia-container'>
+                  <Link to="#"><img src={FaceBookIcon} alt="" className="socialMedia-icon-fb"/></Link>
+                  <Link to="#"><img src={EmailIcon} alt="" className="socialMedia-icon-em"/></Link>
+                  <Link to="#"><img src={LinkedinIcon} alt="" className="socialMedia-icon-ln"/></Link>
                 </span>
                 <DropdownNavItem content={<img src={menuIcon} alt="menu"/>}>
                     <DropdownMenu></DropdownMenu>
@@ -54,7 +59,11 @@ function Navbar() {
                 <DropdownItem><Link to={() => `/2020${Math.floor(Math.random()*1000)}`}>رمضان 2020</Link></DropdownItem>
                 <DropdownItem><Link to={() => `/كل-الأصناف${Math.floor(Math.random()*1000)}`}>
                 كل المسلسلات</Link></DropdownItem>
-                <DropdownItem><Link to="#">اتصل بنا</Link></DropdownItem>
+                <DropdownItem className='socialMedia-container'>
+                  <Link to="#"><img src={FaceBookIcon} alt="" className="socialMedia-icon-fb"/></Link>
+                  <Link to="#"><img src={EmailIcon} alt="" className="socialMedia-icon-em"/></Link>
+                  <Link to="#"><img src={LinkedinIcon} alt="" className="socialMedia-icon-ln"/></Link>
+                </DropdownItem>
             </div>
         )
     }
