d016b69f11626c984b0dd130a6b97e6c4772931c
Aminessid <essidmohamedamin@gmail.com>
2020-07-04 05:33:18 +0000

    enable fullscreen mode in video

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,7 +7,17 @@ const Video = ({videoId, epTitle}) => {
     return (
         <>
             <div className='videoPlayer'>
-                <iframe width='100%' height='100%' src={videoSrc} frameBorder="0" title="video player"></iframe>
+                <iframe 
+                width='100%' 
+                height='100%' 
+                src={videoSrc} 
+                frameBorder="0" 
+                title="video player"
+                allowfullscreen="allowfullscreen"
+                mozallowfullscreen="mozallowfullscreen" 
+                msallowfullscreen="msallowfullscreen" 
+                oallowfullscreen="oallowfullscreen" 
+                webkitallowfullscreen="webkitallowfullscreen"></iframe>
             </div>
             <div className='episode-title'>
                 <p>{epTitle}</p>
